2aa0b7dcecb2019c7b28f3c7e0d89b7060b5f487
p0bs <robin_penfold@yahoo.com>
2025-10-27 14:09:37 +0000

    Update script to work with new package

--- script.R
+++ script.R
@@ -3,92 +3,95 @@
 
 # Data ----
 
-number_sims <- 100000
+number_simulations <- 100000
 
-data("teams")
-data("schedule_thisSeason")
+teams <- PremPredict::teams
+schedule_thisSeason <- PremPredict::schedule_thisSeason
 
-data_results_thisSeason <- get_footballData(
+data_results_thisSeason <- PremPredict::get_footballData(
   value_link = "https://www.football-data.co.uk/mmz4281/2526/E0.csv", 
   table_schedule = schedule_thisSeason, 
   table_teams = teams, 
   value_yearEnd = 2026L
   )
 
-results_combined <- get_results(
+results_combined <- PremPredict::get_results(
   results_thisSeason = data_results_thisSeason, 
   seasons = 1L
 )
 
-game_latest <- calc_game_latest(results = results_combined)
+game_latest <- PremPredict::calc_game_latest(results = results_combined)
 
-results_filtered_19 <- get_results_filtered(
+row_order_latest_data <- results_combined |> 
+  dplyr::filter(match == game_latest)
+
+results_filtered_19 <- PremPredict::get_results_filtered(
   results = results_combined, 
   index_game_latest = game_latest, 
   lookback_rounds = 19L
 )
 
-results_filtered_76 <- get_results_filtered(
+results_filtered_76 <- PremPredict::get_results_filtered(
   results = results_combined, 
   index_game_latest = game_latest, 
   lookback_rounds = 76L
 )
 
 data_table_current <- data_results_thisSeason |> 
-  calc_table_current()
+  PremPredict::calc_table_current()
 
 data_model_19 <- results_filtered_19 |> 
-  model_prepare_frame() |>
-  model_run()
+  PremPredict::model_prepare_frame() |>
+  PremPredict::model_run()
 
 data_model_76 <- results_filtered_76 |> 
-  model_prepare_frame() |>
-  model_run()
+  PremPredict::model_prepare_frame() |>
+  PremPredict::model_run()
 
 data_parameters_unplayed_19 <- data_model_19 |> 
-  model_extract_parameters()
+  PremPredict::model_extract_parameters()
 
-data_model_parameters_unplayed_19 <- model_parameters_unplayed(
+data_model_parameters_unplayed_19 <- PremPredict::model_parameters_unplayed(
   results = results_filtered_19,
   model_parameters = data_parameters_unplayed_19
 )
 
 data_parameters_unplayed_76 <- data_model_76 |> 
-  model_extract_parameters()
+  PremPredict::model_extract_parameters()
 
-data_model_parameters_unplayed_76 <- model_parameters_unplayed(
+data_model_parameters_unplayed_76 <- PremPredict::model_parameters_unplayed(
   results = results_filtered_76,
   model_parameters = data_parameters_unplayed_76
 )
 
-data_simulate_games_19 <- simulate_games(
+data_simulate_games_19 <- PremPredict::simulate_games(
   data_model_parameters_unplayed = data_model_parameters_unplayed_19,
   value_number_sims = number_simulations,
   value_seed = 2602L
 )
 
-data_simulate_standings_19 <- simulate_standings(
+data_simulate_standings_19 <- PremPredict::simulate_standings(
   data_game_simulations = data_simulate_games_19,
   data_table_latest = data_table_current
 )
 
-data_simulate_outcomes_19 <- simulate_outcomes(
+data_simulate_outcomes_19 <- PremPredict::simulate_outcomes(
   data_standings_simulations = data_simulate_standings_19,
   value_number_sims = number_simulations
 )
 
-data_simulate_games_76 <- simulate_games(
+data_simulate_games_76 <- PremPredict::simulate_games(
   data_model_parameters_unplayed = data_model_parameters_unplayed_76,
   value_number_sims = number_simulations,
   value_seed = 2602L
 )
 
-data_simulate_standings_76 <- simulate_standings(
+data_simulate_standings_76 <- PremPredict::simulate_standings(
   data_game_simulations = data_simulate_games_76,
   data_table_latest = data_table_current
 )
 
-data_simulate_outcomes_76 <- simulate_outcomes(
+data_simulate_outcomes_76 <- PremPredict::simulate_outcomes(
   data_standings_simulations = data_simulate_standings_76,
   value_number_sims = number_simulations
 )
@@ -101,6 +104,11 @@ readr::write_csv(
 readr::write_csv(
   x = data_simulate_outcomes_76, 
   file = "projections76.csv"
+)
+
+readr::write_csv(
+  x = row_order_latest_data, 
+  file = "latest_game.csv"
 )
 
 
